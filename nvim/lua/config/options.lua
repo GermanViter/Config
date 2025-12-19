4ad7a7fccf0116bd31301f891f93ccf22e7575b7
GermanViter <german.viter@icloud.com>
2025-12-19 18:31:10 -0500

    style<neovim>: 4 space indentation
    * modified indentation to 4 spaces for better readability of code

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,13 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 
+-- Nombre d'espaces pour une tabulation
+vim.opt.tabstop = 4 -- largeur d'une tabulation
+vim.opt.shiftwidth = 4 -- largeur de l'indentation automatique
+vim.opt.expandtab = true -- utiliser des espaces au lieu de tab
+
+-- Optionnel : pour que le retrait automatique fonctionne bien
+vim.opt.smartindent = true
+
 -- IMPORTANT pour LazyVim
 vim.g.colors_name = "catppuccin"
-
