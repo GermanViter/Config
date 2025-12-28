e9acfcebad0072324d385bbafed1d3db746e63cf
GermanViter <german.viter@icloud.com>
2025-12-28 07:10:08 -0500

    Modified nvim colorscheme to catppuccin
    1. I had to restart the config from scratch bcs it was broken
    2. I had to modify the colorscheme.lua to load catppuccin
    3. I removed everything from nvim/lua/config/optons.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,15 +1,4 @@
--- Options générales
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.termguicolors = true
-
--- Nombre d'espaces pour une tabulation
-vim.opt.tabstop = 4 -- largeur d'une tabulation
-vim.opt.shiftwidth = 4 -- largeur de l'indentation automatique
-vim.opt.expandtab = true -- utiliser des espaces au lieu de tab
-
--- Optionnel : pour que le retrait automatique fonctionne bien
-vim.opt.smartindent = true
-
--- IMPORTANT pour LazyVim
-vim.g.colors_name = "catppuccin"
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+
+    lazy = false, -- 🔴 THIS IS THE KEY LINE
+    priority = 1000, -- load before all other plugins
+
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+    },
+
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+}
